713b2178f455ff569e9678f75b434fbf697c3606
Daniel Olguin <dani.olg@hotmail.com>
2025-09-04 21:53:20 -0400

    feat: add auth controller with register and login methods

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+            'password_confirmation' => 'required|string|min:6'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'validation_errors' => $validator->errors()
+            ], 422);
+        }
+
+        if ($validator->validated()['password'] !== $validator->validated()['password_confirmation']) {
+            return response()->json([
+                'message' => 'Password confirmation does not match'
+            ], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid email or password'
+            ], 401);
+        }
+
+        $user = JWTAuth::user();
+
+        return response()->json([
+            'message' => 'Login successful',
+            'token' => $token,
+            'user' => $user,
+        ], 200);
+    }
+}
