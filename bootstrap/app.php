007d22c2836c601bebf128e6182e2a99a6d94765
Daniel Olguin <dani.olg@hotmail.com>
2025-09-04 23:17:00 -0400

    feat: add custom exception handling for API responses

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__.'/../routes/api.php',
@@ -15,5 +21,34 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        $exceptions->render(function (MethodNotAllowedHttpException $e, $request) {
+            return response()->json([
+                'message' => 'Method not allowed',
+            ], 405);
+        });
+
+        $exceptions->render(function (NotFoundHttpException $e, $request) {
+            return response()->json([
+                'message' => 'Resource not found'
+            ], 404);
+        });
+
+        $exceptions->render(function (AccessDeniedHttpException $e, $request) {
+            return response()->json([
+                'message' => 'Forbidden'
+            ], 403);
+        });
+
+        $exceptions->render(function (UnauthorizedHttpException $e, $request) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        });
+
+        $exceptions->render(function (Throwable $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], 500);
+        });
+    })
+    ->create();
