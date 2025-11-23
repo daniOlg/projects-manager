570be8018fbb79e3069991b768650f238350f38c
Daniel Olguin <dani.olg@hotmail.com>
2025-11-23 20:48:54 -0300

    feat: enhance password validation with confirmation requirement

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,7 +17,8 @@ class AuthController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
+            'password' => 'required|string|min:8|confirmed',
+            'password_confirmation' => 'required|string|min:8',
         ]);
 
         if ($validator->fails()) {
